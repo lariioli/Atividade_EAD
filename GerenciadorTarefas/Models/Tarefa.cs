2508ca42cb8603d5079fb9c399d63c403298229f
Larissa de Oliveira <larissaoliveira3z@gmail.com>
2025-08-19 14:42:16 -0300

    Tarefas

---
+++ GerenciadorTarefas/Models/Tarefa.cs
@@ -0,0 +1,9 @@
+namespace GerenciadorTarefas.Models;
+
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
---
+++ GerenciadorTarefas/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+
+options.UseMySql(
+builder.Configuration.GetConnectionString("DefaultConnection"),
+ServerVersion.AutoDetect(
+builder.Configuration.GetConnectionString("DefaultConnection")
+)
+
+)
+);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthorization(); // Adicione esta linha
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
